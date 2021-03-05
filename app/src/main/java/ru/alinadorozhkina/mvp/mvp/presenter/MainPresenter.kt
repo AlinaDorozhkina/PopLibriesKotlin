825064d8d7d6a2c415a000120572edd7b5ab77e2
app-droid <alinadorozhkina91@mail.ru>
2021-03-05 14:33:46 +0300

    работа над ошибками (исправлено наименование метода презентера)

--- app/src/main/java/ru/alinadorozhkina/mvp/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/ru/alinadorozhkina/mvp/mvp/presenter/MainPresenter.kt
@@ -7,17 +7,17 @@ class MainPresenter(private val view: MainView) {
 
     private val model = CounterModel()
 
-    fun getButton1Value() {
+    fun clickButton1() {
         val nextValue = model.next(0)
         view.setButton1Text(nextValue.toString())
     }
 
-    fun getButton2Value() {
+    fun clickButton2() {
         val nextValue = model.next(1)
         view.setButton2Text(nextValue.toString())
     }
 
-    fun getButton3Value() {
+    fun clickButton3() {
         val nextValue = model.next(2)
         view.setButton3Text(nextValue.toString())
     }
